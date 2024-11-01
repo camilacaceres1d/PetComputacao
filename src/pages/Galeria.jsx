ac4a1e4bf4b8adc365853273404a3c8bda1a618a
bax7os <ana.moraes@ufms.br>
2024-11-01 19:25:02 -0400

    galeria-de-fotos-feito

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,13 +1,90 @@
 import Navbar from "./../components/Navbar/Navbar";
 import Footer from "./../components/Footer/Footer";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Galeria.css';
+import Art_graphs from "./../components/Background_Art/Art_graphs";
 
-function Galeria (){
+function Galeria() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const totalSlides = 5;
 
-    return (
-      <>
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex === totalSlides - 1 ? 0 : prevIndex + 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [totalSlides]);
+
+  const handlePrevClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? totalSlides - 1 : prevIndex - 1));
+  };
+
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex === totalSlides - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <React.Fragment>
       <Navbar/>
-      <Footer/>
-      </>
-    );
+      <div className="background-art">
+        <Art_graphs />
+      </div>
+      <div className="container">
+        <div className="content">
+          <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+              {Array.from({ length: totalSlides }).map((_, index) => (
+                <li
+                  key={index}
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to={index}
+                  className={activeIndex === index ? 'active' : ''}
+                ></li>
+              ))}
+            </ol>
+            <div className="carousel-inner">
+              {['slide1.jpg', 'slide2.jpg', 'slide3.jpg', 'slide4.jpg', 'slide5.jpeg'].map((image, index) => (
+                <div
+                  key={index}
+                  className={`carousel-item ${activeIndex === index ? 'active' : ''}`}
+                >
+                  <img
+                    className="d-block w-100"
+                    src={`src/assets/images/galeria/${image}`}
+                    alt={`Slide ${index + 1}`}
+                  />
+                </div>
+              ))}
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="prev"
+              onClick={handlePrevClick}
+            >
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Anterior</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="next"
+              onClick={handleNextClick}
+            >
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Próximo</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer/>   
+    </React.Fragment>
+  );
 }
+
 export default Galeria;
